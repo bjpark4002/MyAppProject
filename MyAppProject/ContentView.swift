3aa52370332736047defbcc9e318212ca0cea99a
att1667 <att1667@naver.com>
2022-05-28 22:50:27 +0900

    프로젝트 생성

---
+++ MyAppProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAppProject
+//
+//  Created by 최혜지 on 2022/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAppProject/MyAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppProjectApp.swift
+//  MyAppProject
+//
+//  Created by 최혜지 on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct MyAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
